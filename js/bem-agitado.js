85ec7a737d106749d6d2808480f5565ca0cc07c0
Miguel Borges <mrcb08022002@gmail.com>
2022-04-25 19:06:05 +0100

    mudar variaveis e calcular lucro

--- js/bem-agitado.js
+++ js/bem-agitado.js
@@ -3,8 +3,8 @@ function calcula() {
 
     const volume = Number(inputs[0].value);
     const caudal = Number(inputs[1].value);
-    const gliInicial = Number(inputs[2].value);
-    const gluInicial = Number(inputs[3].value);
+    const gli= Number(inputs[2].value);
+    const glu= Number(inputs[3].value);
 
     const MIU_MAX = 0.26;
     const XM = 7.62;
@@ -12,11 +12,16 @@ function calcula() {
     const YP_S_GLI = 0.133;
     const YP_S_GLU = 0.743;
     const YP_X = 3.09;
+    
+    const gliPrice = 0.01708;
+    const gluPrice = 213;
+    const APGPrice = 2500;
+
 
     let miu = caudal / volume;
     let X = XM * (1 - miu / MIU_MAX);
-    let gli = gliInicial - (1 / YX_S + YP_X/YP_S_GLI) * X 
-    let glu = gluInicial - YP_X / YP_S_GLU * X
+    let gliInicial = gli + (1 / YX_S + YP_X/YP_S_GLI) * X;
+    let gluInicial = glu + YP_X / YP_S_GLU * X;
     let P = miu * YP_X * X * volume / caudal;
     let massP = P * caudal;
     let massGli = caudal * gliInicial;
@@ -27,9 +32,17 @@ function calcula() {
     document.querySelector("#massaGlu").textContent = `Massa de glutamato na entrada: ${Math.round(massGlu * 10000) / 10000} g/h`;
     document.querySelector("#APG").textContent = `Concentração de produto no reator: ${Math.round(P * 10000)/10000} g/L`
     document.querySelector("#miu").innerHTML = `Taxa de crescimento: ${Math.round(miu * 10000)/10000} h<sup>-1</sup>`;
-    document.querySelector("#gli").textContent = `Concentração de glicose: ${Math.round(gli * 10000)/10000} g/L`;
-    document.querySelector("#glu").textContent = `Concentração de glutamato: ${Math.round(glu * 10000)/10000} g/L`;
+    document.querySelector("#gliInicial").textContent = `Concentração de glicose à entrada: ${Math.round(gliInicial * 10000)/10000} g/L`;
+    document.querySelector("#gluInicial").textContent = `Concentração de glutamato à entrada: ${Math.round(gluInicial * 10000)/10000} g/L`;
     document.querySelector("#X").textContent = `Concentração de biomassa no reator: ${Math.round(X * 10000)/10000} g/L`;
+
+    let custos = gliPrice * massGli + gluPrice * massGlu;
+    let ganhos = APGPrice * massP;
+    let profit = ganhos - custos;
+
+    document.querySelector("#custo").textContent = `Custos: ${Math.round(custos * 100) / 100}€`;
+    document.querySelector("#ganho").textContent = `Ganhos: ${Math.round(ganhos * 100) / 100}€`;
+    document.querySelector("#profit").textContent = `Profit: ${Math.round(profit * 100) / 100}€`;
 }
 
 document.querySelectorAll("input").forEach((input) => input.addEventListener('change', calcula));
